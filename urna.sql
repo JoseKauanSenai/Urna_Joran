9a17c29e3d71f326b865640dcf4eab69d3168a43
JoseKauanSenai <168464867+JoseKauanSenai@users.noreply.github.com>
2024-10-03 17:10:45 -0300

    Criação do Banco de Dados com o MYSQL

---
+++ urna.sql
@@ -0,0 +1,94 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3307
+-- Tempo de geração: 03/10/2024 às 22:08
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `urna`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `candidatos`
+--
+
+CREATE TABLE `candidatos` (
+  `id` int(2) NOT NULL,
+  `numero_votos` int(2) NOT NULL,
+  `nome` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `votacao`
+--
+
+CREATE TABLE `votacao` (
+  `id` int(11) NOT NULL,
+  `numero_titulo` int(12) NOT NULL,
+  `numero_candidato` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `candidatos`
+--
+ALTER TABLE `candidatos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `votacao`
+--
+ALTER TABLE `votacao`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_numero_candidato` (`numero_candidato`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `candidatos`
+--
+ALTER TABLE `candidatos`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `votacao`
+--
+ALTER TABLE `votacao`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `votacao`
+--
+ALTER TABLE `votacao`
+  ADD CONSTRAINT `fk_numero_candidato` FOREIGN KEY (`numero_candidato`) REFERENCES `candidatos` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
